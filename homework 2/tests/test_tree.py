1381db9d85dc506b088448897c3ce428d18047a4
Serena Zhu <serena@SerenadeMacBook-2.local>
2018-08-22 23:56:01 -0400

    hw2

--- homework 2/tests/test_tree.py
+++ homework 2/tests/test_tree.py
@@ -1,1 +1,73 @@
+import unittest
+from tree.print_tree import Tree, Node
 
+
+class TestTree(unittest.TestCase):
+
+    def setUp(self):
+        self.answer1 = [['1']]
+        self.answer2 = [['|', '|', '|', '1', '|', '|', '|'],
+                        ['|', '2', '|', '|', '|', '3', '|'],
+                        ['4', '|', '5', '|', '6', '|', '7']]
+        self.answer3 = [['|', '|', '|', '|', '|', '|', '|', '1', '|', '|', '|', '|', '|', '|', '|'],
+                        ['|', '|', '|', '2', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|'],
+                        ['|', '3', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|'],
+                        ['4', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|']]
+        self.answer4 = [['|', '|', '|', '|', '|', '|', '|', '1', '|', '|', '|', '|', '|', '|', '|'],
+                        ['|', '|', '|', '2', '|', '|', '|', '|', '|', '|', '|', '3', '|', '|', '|'],
+                        ['|', '|', '|', '|', '|', '4', '|', '|', '|', '5', '|', '|', '|', '|', '|'],
+                        ['|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '6', '|', '|', '|', '|']]
+
+    def test_tree1(self):
+        a = Node(1, None, None)
+        tree1 = Tree(a)
+        assert tree1.printTree(tree1.get_height(a)) == self.answer1
+
+    def test_tree2(self):
+        a = Node(1, None, None)
+        b = Node(2, None, None)
+        c = Node(3, None, None)
+        d = Node(4, None, None)
+        e = Node(5, None, None)
+        f = Node(6, None, None)
+        g = Node(7, None, None)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.left = f
+        c.right = g
+
+        tree2 = Tree(a)
+        assert tree2.printTree(tree2.get_height(a)) == self.answer2
+
+    def test_tree3(self):
+        a = Node(1, None, None)
+        b = Node(2, None, None)
+        c = Node(3, None, None)
+        d = Node(4, None, None)
+
+        a.left = b
+        b.left = c
+        c.left = d
+
+        tree3 = Tree(a)
+        assert tree3.printTree(tree3.get_height(a)) == self.answer3
+
+    def test_tree4(self):
+        a = Node(1, None, None)
+        b = Node(2, None, None)
+        c = Node(3, None, None)
+        d = Node(4, None, None)
+        e = Node(5, None, None)
+        f = Node(6, None, None)
+
+        a.left = b
+        a.right = c
+        b.right = d
+        c.left = e
+        e.right = f
+
+        tree4 = Tree(a)
+        assert tree4.printTree(tree4.get_height(a)) == self.answer4
--- homework 2/tree/print_tree.py
+++ homework 2/tree/print_tree.py
@@ -1,1 +1,87 @@
+import math
 
+
+class Tree(object):
+    def __init__(self, root):
+        self.root = root
+
+    def get_value_root(self):
+        if self.root is not None:
+            return self.root.value
+        else:
+            return None
+
+    def get_height(self, root):
+        if root is None:
+            return 0
+        return max(self.get_height(root.left), self.get_height(root.right)) + 1
+
+    def printTree(self, height):
+        if self.root is None:
+            return
+        out_list = []
+        queue = []
+        queue.append(self.root)
+        rs = []
+        n = 2 << int(height - 1)
+        sp_num = n / 2 - 1
+        num = 1
+        while num:
+            node = queue.pop(0)
+            if node.left is not None:
+                queue.append(node.left)
+                num += 1
+            else:
+                queue.append(Node('|', None, None))
+            if node.right is not None:
+                queue.append(node.right)
+                num += 1
+            else:
+                queue.append(Node('|', None, None))
+            if node.value != '|':
+                num -= 1
+            rs.append(node)
+        out = ''
+        j = 0
+        k = 1
+
+        while rs:
+            node = rs[0]
+            rs = rs[1:]
+            thres = math.log(k, 2)
+
+            if j == thres:
+                if j != 0:
+                    # out += '\n'
+                    out_list.append(out)
+                sp = '|' * sp_num
+                out = sp + str(node.value) + sp
+                j += 1
+                sp_num = (sp_num + 1) / 2 - 1
+            else:
+                out += '|' + sp + str(node.value) + sp
+
+            k += 1
+        add_slush_num = 2 ** height - 1 - len(out)
+        out += '|'*add_slush_num
+        out_list.append(out)
+        for out in out_list:
+            print out
+        return [list(l) for l in out_list]
+
+
+class Node(object):
+    def __init__(self, value, left, right):
+        """
+        Node for a tree,
+
+        ;param value: (float)
+        ;param left: (Node)
+        ;param right: (Node)
+        """
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def get_value(self):
+        return self.value
